a1c106af5d191e070cbb60eb50fccded673cc8cc
Jakub Kielar <jacob.kielar@gmail.com>
2023-12-20 23:04:06 +0100

    Add static model

--- app/src/main/java/com/grandefirano/signtalk/recognition/dictionary/DictionaryProvider.kt
+++ app/src/main/java/com/grandefirano/signtalk/recognition/dictionary/DictionaryProvider.kt
@@ -2,6 +2,7 @@ package com.grandefirano.signtalk.recognition.dictionary
 
 import android.content.Context
 import com.grandefirano.signtalk.ml.Pjm10ModelV2
+import com.grandefirano.signtalk.ml.PjmLeftHandV1
 import com.grandefirano.signtalk.recognition.TranslationChoice
 import dagger.hilt.android.qualifiers.ApplicationContext
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
@@ -33,7 +34,7 @@ class PredictionInterpreterProvider @Inject constructor(
                 TranslationChoice.PJM_POLISH -> PJMPolishActionInterpreter(context)
             }
             false -> when (translationChoice) {
-                TranslationChoice.PJM_POLISH -> PJMPolishActionInterpreter(context)
+                TranslationChoice.PJM_POLISH -> PJMPolishStaticInterpreter(context)
             }
         }
     }
@@ -52,8 +53,7 @@ class PJMPolishActionInterpreter(context: Context) : Interpreter {
 }
 
 class PJMPolishStaticInterpreter(context: Context) : Interpreter {
-    private val model = Pjm10ModelV2.newInstance(context)
-
+    private val model = PjmLeftHandV1.newInstance(context)
     override fun interpret(input: TensorBuffer): TensorBuffer {
         return model.process(input).outputFeature0AsTensorBuffer
     }
