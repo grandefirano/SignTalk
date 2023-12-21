98b6c1cb1b8479d1705b5d327df06fe353d6bda7
Jakub Kielar <jacob.kielar@gmail.com>
2023-12-21 02:02:21 +0100

    Add bigger model for letters and numbers

--- app/src/main/java/com/grandefirano/signtalk/recognition/dictionary/DictionaryLanguage.kt
+++ app/src/main/java/com/grandefirano/signtalk/recognition/dictionary/DictionaryLanguage.kt
@@ -5,5 +5,5 @@ fun getPolishActionDictionary(): List<String> {
 }
 
 fun getPolishStaticDictionary(): List<String> {
-    return listOf("1", "2", "3")
+    return listOf("1", "2", "3","4","5","A","B","C","E","I","L","M","N","O","P","R","S","T","U","W","Y")
 }
--- app/src/main/java/com/grandefirano/signtalk/recognition/dictionary/DictionaryProvider.kt
+++ app/src/main/java/com/grandefirano/signtalk/recognition/dictionary/DictionaryProvider.kt
@@ -2,7 +2,7 @@ package com.grandefirano.signtalk.recognition.dictionary
 
 import android.content.Context
 import com.grandefirano.signtalk.ml.Pjm10ModelV2
-import com.grandefirano.signtalk.ml.PjmLeftHandV1
+import com.grandefirano.signtalk.ml.PjmLeftHandV2
 import com.grandefirano.signtalk.recognition.TranslationChoice
 import dagger.hilt.android.qualifiers.ApplicationContext
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
@@ -53,7 +53,7 @@ class PJMPolishActionInterpreter(context: Context) : Interpreter {
 }
 
 class PJMPolishStaticInterpreter(context: Context) : Interpreter {
-    private val model = PjmLeftHandV1.newInstance(context)
+    private val model = PjmLeftHandV2.newInstance(context)
     override fun interpret(input: TensorBuffer): TensorBuffer {
         return model.process(input).outputFeature0AsTensorBuffer
     }
