94e605c5f434b8511130104d1e5b1d515ada6d2c
Jakub Kielar <jacob.kielar@gmail.com>
2024-05-30 15:52:10 +0200

    Add time measurement

--- app/src/main/java/com/grandefirano/signtalk/prediction/interpreter/pjm/PJMPolishActionInterpreter.kt
+++ app/src/main/java/com/grandefirano/signtalk/prediction/interpreter/pjm/PJMPolishActionInterpreter.kt
@@ -4,11 +4,28 @@ import android.content.Context
 import com.grandefirano.signtalk.ml.Pjm21CompleteLstmModel
 import com.grandefirano.signtalk.prediction.interpreter.ActionInterpreter
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 class PJMPolishActionInterpreter(context: Context) : ActionInterpreter {
     private val model = Pjm21CompleteLstmModel.newInstance(context)
 
+    val times = mutableListOf<Long>()
+
+    val averageTime:Double
+        get() = times.average()
+
+    @OptIn(ExperimentalTime::class)
     override fun interpret(input: TensorBuffer): TensorBuffer {
-        return model.process(input).outputFeature0AsTensorBuffer
+//        val start = System.currentTimeMillis()
+        var value: TensorBuffer
+        val time = measureTime {
+             value = model.process(input).outputFeature0AsTensorBuffer
+        }
+        times.add(time.inWholeMicroseconds)
+        //println("times actual $time average $averageTime")
+        //3.151
+        if(times.size == 300) println("test of 300 average $averageTime")
+        return value
     }
 }
--- app/src/main/java/com/grandefirano/signtalk/prediction/interpreter/pjm/PJMPolishStaticInterpreter.kt
+++ app/src/main/java/com/grandefirano/signtalk/prediction/interpreter/pjm/PJMPolishStaticInterpreter.kt
@@ -4,10 +4,28 @@ import android.content.Context
 import com.grandefirano.signtalk.ml.Pjm21HandFnnModel
 import com.grandefirano.signtalk.prediction.interpreter.StaticInterpreter
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 class PJMPolishStaticInterpreter(context: Context) : StaticInterpreter {
     private val model = Pjm21HandFnnModel.newInstance(context)
+
+    val times = mutableListOf<Long>()
+
+    val averageTime:Double
+        get() = times.average()
+
+
+    @OptIn(ExperimentalTime::class)
     override fun interpret(input: TensorBuffer): TensorBuffer {
-        return model.process(input).outputFeature0AsTensorBuffer
+        var value: TensorBuffer
+        val time = measureTime {
+            value = model.process(input).outputFeature0AsTensorBuffer
+        }
+        times.add(time.inWholeMicroseconds)
+        //println("ST times actual $time average $averageTime")
+        //3.151
+        if(times.size == 300) println("ST test of 300 average $averageTime")
+        return value
     }
 }
